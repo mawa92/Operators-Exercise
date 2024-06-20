921fdd8da0f6837fa46d8cf8e78b9ba980f5e69d
Mawa Ali <mawacoding1@gmail.com>
2024-06-20 03:13:40 -0400

    inital commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+
+            int diff = a - b;
+
+            int product = a * b;
+
+            int quotient = a / b;
+
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+            Console.WriteLine($"{a}+{b} is {sum}");
+            Console.WriteLine($"{a}-{b} is {diff}");
+            Console.WriteLine($"{a}*{b} is {product}");
+
+            Console.WriteLine("Please enter the radius of your ciricle");
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+            Console.WriteLine(AreaOfCircle(radius));
+
+        }
+
+        private static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
         }
     }
 }
